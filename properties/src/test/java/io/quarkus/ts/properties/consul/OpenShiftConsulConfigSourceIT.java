3e39df2046f032d09464e898bfd5b614de97523f
Ram <157704087+ramdr@users.noreply.github.com>
2025-04-24 11:19:25 +0530

    Update OpenShiftConsulConfigSourceIT.java

--- properties/src/test/java/io/quarkus/ts/properties/consul/OpenShiftConsulConfigSourceIT.java
+++ properties/src/test/java/io/quarkus/ts/properties/consul/OpenShiftConsulConfigSourceIT.java
@@ -5,6 +5,5 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
-@DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "consul container not available on s390x & ppc64le.")
 public class OpenShiftConsulConfigSourceIT extends ConsulConfigSourceIT {
 }
